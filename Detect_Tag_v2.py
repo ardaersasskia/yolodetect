3c93ad9bc99b72e08d14c03d8cbcdb0ef4186df8
袁晓威 <yuanxiaowei135@163.com>
2024-03-04 21:23:23 +0800

    1.0 更改为多线程

--- Detect_Tag_v2.py
+++ Detect_Tag_v2.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from threading import Thread
+import queue
 import cv2
 import numpy as np
-import collections
 import torch
-import time
-import os
-# from white_balance import white_balance
+import os,time
 
+# ... (其余import和functions，保持不变)
 def circshift(matrix, shiftnum1, shiftnum2):
     h, w = matrix.shape
     matrix = np.vstack((matrix[(h - shiftnum1):, :], matrix[:(h - shiftnum1), :]))
@@ -15,10 +13,8 @@ def circshift(matrix, shiftnum1, shiftnum2):
     return matrix
 
 class Detect_Tag:
-    def __init__(self, img) -> None:
-        self.img = img
-        kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], np.float32)
-        self.img = cv2.filter2D(self.img, -1, kernel=kernel)
+    def __init__(self) -> None:
+        pass
     def boxarea(self, box):
         x1 = box[0][0]
         x2 = box[3][0]
@@ -52,7 +48,7 @@ class Detect_Tag:
 
         # 顶点坐标旋转（右下 左下 左上 右上）
         box_2 = circshift(box, 2 - minindex, 0)
-        print('外接矩形box:', box_2)
+        #print('外接矩形box:', box_2)
         box_2 = np.array(box_2, dtype=np.double)
 
         # retval_1,rvec_1,tvec_1,_ = cv2.solvePnPRansac(objPoints, box_2, cameraMatrix, distCoeffs, False, cv2.SOLVEPNP_P3P)
@@ -75,7 +71,7 @@ class Detect_Tag:
 
         # 顶点坐标旋转（右下 左下 左上 右上）
         box_2 = circshift(box, 2 - minindex, 0)
-        print('外接矩形box:', box_2)
+        #print('外接矩形box:', box_2)
         box_2 = np.array(box_2, dtype=np.double)
 
         # retval_1,rvec_1,tvec_1,_ = cv2.solvePnPRansac(objPoints, box_2, cameraMatrix, distCoeffs, False, cv2.SOLVEPNP_P3P)
@@ -89,67 +85,92 @@ class Detect_Tag:
         return yaw, pitch, roll, True
 
     # 处理图片
-    def get_location(self, color, yolobox, cameraMatrix, distCoeffs, retVal = 0):
-        # 转为灰度图像
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        # 进行高斯模糊
-        gr = cv2.GaussianBlur(gray, (3, 3), 0)
-        # 将高斯模糊灰度图转换为二值图，参数ret为阈值，参数binary为转换后的二值图
-        ret, binary = cv2.threshold(gr, 80, 255, cv2.THRESH_BINARY_INV)  # outdoor: 200
-        # 寻找轮廓，contours为轮廓点，hierarchy为轮廓层次信息
-        # hierarchy[0][i][y]
-        # i为这组轮廓点的编号
-        # y=0表示同一层次下一个轮廓编号
-        # y=1表示同一层次上一个轮廓编号
-        # y=2表示它的第一个子轮廓编号
-        # y=3表示它的父轮廓编号
-        contours, hierarchy = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-
-        w, h, _ = self.img.shape
+    def get_location(self, contours, hierarchy, yolobox, cameraMatrix, distCoeffs, retVal=0):
         final_contour = []
 
         for ic in range(len(contours)):
-            # 取出一组轮廓点
             contour = contours[ic]
-            # 计算其包围面积
             rect_area = cv2.contourArea(contour)
-            # 计算轮廓的近似多边形  
             approx = cv2.approxPolyDP(contour, .03 * cv2.arcLength(contour, True), True)
-            # 如果approx的点数为4说明这个多边形为四边形  为什么面积判断是1000而不是采用yolo识别框面积的50%
-            if rect_area > self.boxarea(yolobox)*0.3 and len(approx) == 4:
-                # 计算这个轮廓的矩
-                # m00为面积
+            if rect_area > self.boxarea(yolobox) * 0.2 and len(approx) == 4:
                 M = cv2.moments(contour)
-                # 计算中心坐标x,y
                 x = int(M['m10'] / M['m00'])
                 y = int(M['m01'] / M['m00'])
-                # 如果轮廓中心在yolobox的中心范围内
                 if self.inside(yolobox, x, y):
-                    # 测试发现在将hierarchy[0][ic][0] > -1改为hierarchy[0][ic][0] == -1时可以将多个框轮廓识别出来
-                    # 认为应该将判断条件改为hierarchy[0][ic][0] == -1 and hierarchy[0][ic][2] == -1 
-                    # 即选中每层内框的最后一个轮廓，判断是否是红色的内框使用面积是否大于yolo识别框面积的50%
-                    if hierarchy[0][ic][0] == -1 and hierarchy[0][ic][2] == -1:
+                    if hierarchy[0][ic][2] == -1:
                         final_contour = contour
-                        cv2.drawContours(self.img, final_contour, -1, (50, 255, 255), 3)
-                        cv2.namedWindow("contours",0)
-                        cv2.resizeWindow("contours", 1272, 972)
-                        cv2.imshow('contours', self.img)
-                        cv2.waitKey(0)
 
         if len(final_contour) < 1:
             print("no contour detected!")
             rect_r = cv2.minAreaRect(yolobox)
             yolobox = cv2.boxPoints(rect_r)
             return self.gettvec(yolobox, 3, cameraMatrix, distCoeffs)
-
-        rect = cv2.minAreaRect(final_contour)  # cv::RotatedRect
-        self.box_1 = cv2.boxPoints(rect)  # cv::Rect   左上 右上 右下 左下
+        print("contour detected!")
+        rect = cv2.minAreaRect(final_contour)
+        self.box_1 = cv2.boxPoints(rect)
 
         if retVal == 0:
             return self.gettvec(self.box_1, 2.3, cameraMatrix, distCoeffs)
         elif retVal == 1:
             return self.getRvec(self.box_1, 2.3, cameraMatrix, distCoeffs)
+class FileRead(Thread):
+    def __init__(self, path,yolo_img_queue,opencv_img_queue):
+        Thread.__init__(self)
+        self.path = path
+        self.yolo_img_queue = yolo_img_queue
+        self.opencv_img_queue = opencv_img_queue      
 
+    def run(self):
+        files = os.listdir(self.path)
+        for file in files:
+            img_path = os.path.join(path, file)
+            img = cv2.imread(img_path)       
+            self.yolo_img_queue.put(img)
+            self.opencv_img_queue.put(img)
+        self.yolo_img_queue.put(None)  # 发送None作为YOLO线程的停止信号
+        self.opencv_img_queue.put(None)  # 发送None作为OpenCV线程的停止信号
+
+class YOLODetectThread(Thread):
+    def __init__(self, img_queue, result_queue, model):
+        Thread.__init__(self)
+        self.img_queue = img_queue
+        self.result_queue = result_queue
+        self.model = model
+        
+
+    def run(self):
+        while True:
+            img = self.img_queue.get()
+            if img is None:
+                self.result_queue.put(None)
+                break
+            results = self.model(img)
+            self.result_queue.put(results.xyxy[0].to('cpu').numpy())
+
+class OpenCVProcessThread(Thread):
+    def __init__(self, img_queue, result_queue, cameraMatrix, distCoeffs):
+        Thread.__init__(self)
+        self.img_queue = img_queue
+        self.result_queue = result_queue
+        self.cameraMatrix = cameraMatrix
+        self.distCoeffs = distCoeffs
+
+    def run(self):
+        while True:
+            img = self.img_queue.get()
+            if img is None:
+                self.result_queue.put(None)
+                break
+             # 转为灰度图像
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # 进行高斯模糊
+            blured = cv2.GaussianBlur(gray, (3, 3), 0)
+        # 二值化
+            _, binary = cv2.threshold(blured, 80, 255, cv2.THRESH_BINARY_INV)
+        # 查找轮廓
+            contours, hierarchy = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        # 将结果放入队列
+            self.result_queue.put((contours, hierarchy))
 
 
 if __name__ == '__main__':
@@ -160,13 +181,13 @@ if __name__ == '__main__':
     cx = 1.23265844e+03
     cy = 9.45947471e+02
     # 畸变系数
+    cameraMatrix = np.array([[fx, 0, cx],
+                             [0, fy, cy],
+                             [0, 0, 1]], dtype=np.double)
     distCoeffs = np.array([[-1.28826679e-01, 4.12884075e-01, 2.12200329e-03, 7.82174557e-04, -9.14304350e-01]],
                           dtype=np.double)
 
-    K = np.array([[fx, 0, cx],
-                  [0, fy, cy],
-                  [0, 0, 1]], dtype=np.double)
-    cameraMatrix = K
+    # 初始化队列和设备
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -175,43 +196,59 @@ if __name__ == '__main__':
                            source='local', force_reload=False)
     model = model.to(device)
 
-    path = "dataset\\TAG\\display"
-    files = os.listdir(path)
-    for i, file in enumerate(files):
-        img_path = os.path.join(path, file)
-        img = cv2.imread(img_path)
-        results = model(img_path)
-        # 过滤模型，获取classItem=0的识别结果
-        xmins = results.pandas().xyxy[0]['xmin']
-        ymins = results.pandas().xyxy[0]['ymin']
-        xmaxs = results.pandas().xyxy[0]['xmax']
-        ymaxs = results.pandas().xyxy[0]['ymax']
-        classList = results.pandas().xyxy[0]['class']
-        confidences = results.pandas().xyxy[0]['confidence']
-        newList = []
-        for xmin, ymin, xmax, ymax, classItem, conf in zip(xmins, ymins, xmaxs, ymaxs, classList, confidences):
-            if classItem == 0 and conf > 0.3:
-                newList.append([int(xmin), int(ymin), int(xmax), int(ymax), conf])
+    yolo_img_queue = queue.Queue()
+    opencv_img_queue = queue.Queue()
+    yolo_result_queue = queue.Queue()
+    opencv_result_queue = queue.Queue()
 
-        if len(newList) > 0:
-            listItem = newList[0]
-            cv2.rectangle(img, (listItem[0], listItem[1]), (listItem[2], listItem[3]), (50, 255, 50), 3,
-                          lineType=cv2.LINE_AA)
 
-            rect = np.array([[listItem[0], listItem[1]], [listItem[0], listItem[3]],
-                             [listItem[2], listItem[1]], [listItem[2], listItem[3]]], dtype=np.int32)
+    path = "dataset/TAG/display"
+    yolo_thread = YOLODetectThread(yolo_img_queue, yolo_result_queue, model)
+    opencv_thread = OpenCVProcessThread(opencv_img_queue, opencv_result_queue, cameraMatrix, distCoeffs)
+    file_thread = FileRead(path,yolo_img_queue,opencv_img_queue)
+    file_thread.start()
+    time.sleep(1)
+    yolo_thread.start()
+    time.sleep(1)
+    opencv_thread.start()
+    time.sleep(1)
+    while True:
+        # 等待两个线程的结果
+        t1=time.time()
+        yolo_result = yolo_result_queue.get()
+        opencv_result = opencv_result_queue.get()
+        
+        if yolo_result is not None and opencv_result is not None:
+            detections_yolo = yolo_result
+            (contours_opencv, hierarchy_opencv) = opencv_result
+            # 过滤模型，获取classItem=0的识别结果
+            newList = []
+            for detection in detections_yolo:
+                xmin, ymin, xmax, ymax, conf, classItem = detection[:6]
+                if int(classItem) == 0 and conf > 0.3:
+                    newList.append([int(xmin), int(ymin), int(xmax), int(ymax), conf])
 
-            a = Detect_Tag(img)
-            result = a.get_location("red", rect, cameraMatrix, distCoeffs)
-            print("result: ", result)
-            print("图片名称：", file)
-            print("")
+            if len(newList) > 0:
+                listItem = newList[0]
+                rect = np.array([[listItem[0], listItem[1]], [listItem[0], listItem[3]],
+                                 [listItem[2], listItem[1]], [listItem[2], listItem[3]]], dtype=np.int32)
+                a = Detect_Tag()
 
-            img = cv2.resize(img, (1272, 972))
-            cv2.imshow(file, img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-        else:
-            print("No detection:" + file)
+                # Use the contours and hierarchy to finalize the location
+                result_location = a.get_location(contours_opencv, hierarchy_opencv, rect, cameraMatrix, distCoeffs)
+                #print(result_location)
+                #cv2.waitKey(0)
+                # cv2.destroyAllWindows()
+            else:
+                print("No detection")
+        elif yolo_result == None and opencv_result == None:
+            break
+        t2 = time.time()
+        print("time:", t2 - t1)
+
+    # 等待所有进程完成
+    yolo_thread.join()
+    opencv_thread.join()
+    
 
 
---
+++ test_camera.py
@@ -0,0 +1,18 @@
+# import cv2,time
+# cap=cv2.VideoCapture(0)
+# # while True:
+# #     t0=time.time()
+# #     ret,frame=cap.read()
+# #     t1=time.time()
+# #     cv2.imshow("frame",frame)
+# #     cv2.waitKey(16)
+# #     if ret:
+# #         print("读取视频帧时间：",t1-t0)
+# # ret,frame=cap.read()
+# # print("read first")
+# cv2.waitKey(5000)
+# t0=time.time()
+# ret,frame=cap.read()
+# t1=time.time()
+# print("读取视频帧时间：",t1-t0)
+
